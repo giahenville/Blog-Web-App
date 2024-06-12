ce8bdcb3fc5f57706e17b52163c8a5f826f4a586
Gia Henville <140769584+giabui@users.noreply.github.com>
2024-06-12 15:36:02 -0400

    feat: Add partials to home, about, and create pages

--- Blog Web App/index.js
+++ Blog Web App/index.js
@@ -4,7 +4,9 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+
 app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
