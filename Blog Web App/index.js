7157d029d12ee77ed5809e8fcea1180fb1b362ca
Gia Henville <140769584+giabui@users.noreply.github.com>
2024-06-19 12:28:36 -0400

    feat: Add search by title search-bar functionality to profile page

--- Blog Web App/index.js
+++ Blog Web App/index.js
@@ -1,16 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const posts = [];
 let totalPosts = 0;
-let postDate = new Date().toDateString(); 
-
+ 
+// Middleware
 app.use(bodyParser.urlencoded( {extended: true} ));
 app.use(express.static("public"));
 
+// Set the views directory and view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Routes
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
@@ -24,22 +33,21 @@ app.get("/create", (req, res) => {
 });
 
 app.get("/profile", (req, res) => {
+    let postDate = new Date().toDateString();
     res.render("profile.ejs", {posts: posts, totalPosts: totalPosts, postDate: postDate});
 });
 
 app.post("/submit", (req, res) => {
-    const title = req.body["title"];
-    const body = req.body["body"];
-    const image = req.body["image"];
-    const topic = req.body["topic"];
+    const { title, body, image, topic } = req.body;
 
-    const newPost = { title: title, body: body, image: image, topic: topic };
+    const newPost = { title, body, image, topic, date : new Date().toDateString() };
     posts.push(newPost);
     totalPosts++;
 
     res.redirect("/profile");
 });
 
+// Start server
 app.listen(port, () => {
     console.log("Blog Web App running on port ", port);
 });
