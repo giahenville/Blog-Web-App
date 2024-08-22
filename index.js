939f49e5564b9dbe0ac194d6521d6eed4db84f6e
Gia Henville <140769584+giabui@users.noreply.github.com>
2024-08-21 23:10:36 -0400

    feat: Add auth for user to login and register

--- index.js
+++ index.js
@@ -28,8 +28,10 @@ app.use(
     })
 );
 
-let posts = [];
+// TODO: get rid of this and make sure info comes from database
+// let posts = [];
 let totalPosts = 0;
+
 // state checker to see if we are editing a post
 let isEditing = false;
 let post;
@@ -59,37 +61,59 @@ const db = new pg.Client({
 });
 db.connect();
 
-// Routes
+////////////
+// ROUTES //
+////////////
+
+///////////////
+// HOME PAGE //
+///////////////
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+
+///////////
+// ABOUT //
+///////////
 app.get("/about", (req, res) => {
     res.render("about.ejs");
 });
 
+
+///////////////////////////////////////
+////////// USER POSTS LOGIC ///////////
+///////////////////////////////////////
+// send user to create page
 app.get("/create", (req, res) => {
     res.render("create.ejs");
 });
 
-app.get("/profile", (req, res) => {
-    console.log(req.user);
-    // access database and get all the posts that match the email
-    res.render("profile.ejs", {posts: posts, totalPosts: totalPosts});
+// creates new post
+app.post("/submit", async (req, res) => {
+  const { title, body, topic } = req.body; // TODO: where is emaiil?
+  const email = req.user.email;
+  const date = new Date().toDateString();
+  totalPosts++;
+
+  try {
+    // TODO: Create a query to insert into postinfo table
+    const result = await db.query(
+      "INSERT INTO postinfo (email, title, body, topic, date) \
+      VALUES ($1, $2, $3, $4, $5)",
+      [email, title, body, topic, date]
+      );
+      res.redirect("/profile");
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.redirect("/profile");
 });
 
-app.get("/login", (req, res) => {
-    res.render("login.ejs");
-});
-
-app.get("/register", (req, res) => {
-    res.render("register.ejs");
-});
-
+// find post by id
 app.get("/post/:id", (req, res) => {
-    // get the post id from the req
     postId = req.params.id;
-    // find the post the user is trying to access from the posts array
     post = posts.find(p => p.id === postId);
     if (post) {
         res.render("post.ejs", {post: post});
@@ -98,6 +122,7 @@ app.get("/post/:id", (req, res) => {
     }
 });
 
+// edit post by id
 app.get("/edit/:id", (req, res) => {
     isEditing = true; 
     // get the post id from the req
@@ -111,73 +136,7 @@ app.get("/edit/:id", (req, res) => {
     }
 });
 
-// logs in user 
-app.post(
-    "/login",
-    passport.authenticate("local", {
-      successRedirect: "/profile",
-      failureRedirect: "/login",
-    })
-  );
-
-// register new user
-app.post("/register", async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-  
-    try {
-      const checkResult = await db.query("SELECT * FROM logincredentials WHERE email = $1", [
-        email,
-      ]);
-      console.log("checkResult is:", checkResult)
-      // checks if user is already registered
-      if (checkResult.rows.length > 0) {
-        req.redirect("/login");
-      } 
-      // creates new user and adds to database and encrypts password
-      else {
-        bcrypt.hash(password, saltRounds, async (err, hash) => {
-          if (err) {
-            console.error("Error hashing password:", err);
-          } else {
-            const result = await db.query(
-              "INSERT INTO  logincredentials (email, password) VALUES ($1, $2) RETURNING *",
-              [email, hash]
-            );
-            const user = result.rows[0];
-            req.login(user, (err) => {
-              console.log("success");
-              res.redirect("/profile");
-            });
-          }
-        });
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  });
-  
-// creates new post
-app.post("/submit", (req, res) => {
-    const { title, body, image, topic } = req.body;
-
-    const newPost = { id: uuidv4(), title, body, image, topic, date : new Date().toDateString() };
-    posts.push(newPost);
-    totalPosts++;
-
-    res.redirect("/profile");
-});
-
-//Deletes post from posts array
-app.post('/delete/:id', (req, res) => {
-    postId = req.params.id;
-    // remove the deleted item from the posts array
-    posts = posts.filter(p => p.id !== postId); 
-    // update posts count
-    totalPosts --;
-    res.redirect("/profile");
-});
-
+// updates post
 app.post("/update", (req, res) => {
     const { title, body, image, topic } = req.body;
     
@@ -198,9 +157,201 @@ app.post("/update", (req, res) => {
     res.redirect("/profile");
 })
 
-
-// Start server
-app.listen(port, () => {
-    console.log("Blog Web App running on port ", port);
+// delete post by id
+app.post('/delete/:id', (req, res) => {
+    postId = req.params.id;
+    // remove the deleted item from the posts array
+    posts = posts.filter(p => p.id !== postId); 
+    // update posts count
+    totalPosts --;
+    res.redirect("/profile");
 });
 
+
+/////////////
+// PROFILE //
+/////////////
+app.get("/profile", async (req, res) => {
+  console.log(req.user);
+  // TODO: access database and get all the posts that match the email
+  // TODO: Test this functionality!!!
+  ////////////////UPDATED GET SECRETS ROUTE/////////////////
+  if (req.isAuthenticated()) {
+    try {
+      const posts = await db.query("SELECT * FROM postinfo WHERE email = $1", [
+        req.user.email,
+      ]);
+      console.log("posts.rows: ", posts.rows);
+      res.render("profile.ejs", { posts: posts.rows, totalPosts: totalPosts });
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    res.redirect("/login");
+  }
+});
+
+
+///////////
+// LOGIN //
+///////////
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+
+/////////////////////////////////////////////
+//////////// REGISTERATION LOGIC ////////////
+/////////////////////////////////////////////
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const checkResult = await db.query(
+      "SELECT * FROM logincredentials WHERE email = $1",
+      [email]
+    );
+    console.log("checkResult is:", checkResult);
+    // checks if user is already registered
+    if (checkResult.rows.length > 0) {
+      req.redirect("/login");
+    }
+    // creates new user and adds to database and encrypts password
+    else {
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.error("Error hashing password:", err);
+        } else {
+          const result = await db.query(
+            "INSERT INTO  logincredentials (email, password) VALUES ($1, $2) RETURNING *",
+            [email, hash]
+          );
+          const user = result.rows[0];
+          req.login(user, (err) => {
+            console.log("success");
+            res.redirect("/profile");
+          });
+        }
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//////////////////////////////////////////////////////
+// PASSPORT MIDDLEWARE AUTHENTICATION CONFIGURATION //
+//////////////////////////////////////////////////////
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    // gets access to user's google profile and email
+    scope: ["profile", "email"],
+  })
+);
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", {
+    successRedirect: "/profile",
+    failureRedirect: "/login",
+  })
+);
+
+// authenticates user login
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/login",
+  })
+);
+
+/////////////////////////////
+// SETTING UP PASSPORT USE //
+/////////////////////////////
+passport.use(
+  "local",
+  new Strategy(async function verify(email, password, cb) {
+    try {
+      console.log("looking for passwords");
+      const result = await db.query(
+        "SELECT * FROM logincredentials WHERE email = $1 ",
+        [email]
+      );
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedHashedPassword = user.password;
+        bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+          if (err) {
+            console.log("Error comparing passwords:", err);
+            return cb(err);
+          } else {
+            if (valid) {
+              console.log("Password is valid!");
+              return cb(null, user);
+            } else {
+              console.log("Password is NOT valid!");
+              return cb(null, false);
+            }
+          }
+        });
+      } else {
+        return cb("User not found");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  })
+);
+
+passport.use(
+  "google",
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      try {
+        const result = await db.query(
+          "SELECT * FROM logincredentials WHERE email = $1",
+          [profile.email]
+        );
+        if (result.rows.length === 0) {
+          const newUser = await db.query(
+            "INSERT INTO logincredentials (email, password) VALUES ($1, $2)",
+            [profile.email, "google"]
+          );
+          return cb(null, newUser.rows[0]);
+        } else {
+          return cb(null, result.rows[0]);
+        }
+      } catch (err) {
+        return cb(err);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+});
+
+//////////////////
+// START SERVER //
+//////////////////
+app.listen(port, () => {
+  console.log("Blog Web App running on port ", port);
+});
