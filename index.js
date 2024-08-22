8872b7aebf6386c91eb8de7d010e2c300cd04f2a
Gia Henville <140769584+giabui@users.noreply.github.com>
2024-08-21 20:39:56 -0400

    feat: Import database and auth dependencies

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import path from "path";
 import { v4 as uuidv4 } from 'uuid';  // Import the UUID package
+import pg from "pg";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const saltRounds = 10;
+env.config();
 
 let posts = [];
 let totalPosts = 0;
