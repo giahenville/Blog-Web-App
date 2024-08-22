559a6808ac2e55f8e53d938394ab76565f160d7f
Gia Henville <140769584+giabui@users.noreply.github.com>
2024-08-21 20:43:36 -0400

    feat: Create sql tables

---
+++ sql/create_loginCredentials_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE loginCredentials (
+	id SERIAL PRIMARY KEY,
+	email VARCHAR(100) NOT NULL UNIQUE,
+	password VARCHAR(100)
+) 
---
+++ sql/create_postInfo_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE postInfo (
+	id SERIAL PRIMARY KEY, 
+	email VARCHAR(100) NOT NULL,
+	title TEXT NOT NULL,
+	body TEXT NOT NULL,
+	topic VARCHAR(25) NOT NULL,
+	FOREIGN KEY (email) REFERENCES loginCredentials(email);
+);
